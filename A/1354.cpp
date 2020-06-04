9433a3de8313ec4012dfe0c3b13dc3f7eb404c60
Sourab Hossain <32600240+sourabhossain@users.noreply.github.com>
2020-06-05 01:32:11 +0600

    Update 1354.cpp

--- A/1354.cpp
+++ A/1354.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    int T;
+
+    scanf("%d", &T);
+
+    while(T--) {
+        long long a, b, c, d;
+
+        scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
+
+        if(b >= a) {
+            printf("%lld\n", b);
+        } else if(d >= c) {
+            puts("-1");
+        } else {
+            printf("%lld\n", (a-b+c-d-1) / (c-d) * c+b);
+        }
+    }
+
+    return 0;
+}
